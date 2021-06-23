eb0035a07313ddecc34a004381e539c06384d075
unknown <gayathriletters@gmail.com>
2021-06-23 18:27:42 +0530

    2nd commit  map addded

--- public/Js/getrequest.js
+++ public/Js/getrequest.js
@@ -27,7 +27,14 @@
 // }
 
 //  ensuring document is ready before working on it
+let map;
 
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  });
+}
 $(document).ready(function(){
 $()
     $('#search').click(function(){
